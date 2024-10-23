823dd4d6552dfe6d5b319b605332fb5679759700
PanickBoi <159054752+PanickBoi@users.noreply.github.com>
2024-10-23 23:55:01 +0300

    no
    afa

--- src/system/Main.java
+++ src/system/Main.java
@@ -7,74 +7,60 @@ import java.util.ArrayList;
 
 public class Main{
 	
-	String myPassword;
-	
-	static void showCreds(Users user) {
-		String[] Roles = { "Student", "Teacher", "Secretariat", "Administration" };
-		System.out.println("User Email: " + user.Email);
-		System.out.println("User Name: " + user.WholeName);
-		System.out.println("User ID: " + user.ID);
-		System.out.println("User Access Level: " + Roles[user.AccessLevel]);
-	}
-	//Temp PolyMethod Location
-	static void addUser(ArrayList<Users> UserList, Student Student) {
-		Student USER = new Student(Student.WholeName,Student.Email,Student.Password,Student.AccessLevel,Student.ID);
-		UserList.add(UserList.size(),USER);
-	}
-
-	static void addUser(ArrayList<Users> UserList, Teacher Teacher) {
-		Teacher USER = new Teacher(Teacher.WholeName,Teacher.Email,Teacher.Password,Teacher.AccessLevel,Teacher.ID);
-		UserList.add(UserList.size(),USER);
-	}
-
-	static void addUser(ArrayList<Users> UserList,Secretariat Secretariat) {
-		Secretariat USER = new Secretariat(Secretariat.WholeName,Secretariat.Email,Secretariat.Password,Secretariat.AccessLevel,Secretariat.ID);
-		UserList.add(UserList.size(),USER);
-	}
-
-	static void addUser(ArrayList<Users> UserList,Administration Administration) {
-		Administration USER = new Administration(Administration.WholeName,Administration.Email,Administration.Password,Administration.AccessLevel,Administration.ID);
-		UserList.add(UserList.size(),USER);
-		
-	}
-	
 	public static void main(String[] args) {
 		// DB Deseriliazation
 
 		
-		ArrayList<Users> UserList = new ArrayList<Users>();
-		Student student = new Student("PanickBoi", "Panickb@gmail.com", "1234", (byte) 0,UserList.size()+1);
-		student.pf();
+		Student student = new Student("PanickBoi", "Panickb@gmail.com", "1234", (byte) 0,0);
+		//student.pf();
 		// Fake Dummy User
-		Administration fakeUser = new Administration("Koutelos", "Koutelos@gmail.com", "Koutoulakidion", (byte) 3,UserList.size()+1);
+		Administration fakeUser = new Administration("Koutelos", "Koutelos@gmail.com", "Koutoulakidion", (byte) 3,1);
 		
-		addUser(UserList,student);
-		addUser(UserList,fakeUser);
+		Users.UserList.add(student);
+		Users.UserList.add(fakeUser);
 		// Introduction // 
 		
 		Scanner myScanner = new Scanner(System.in);
 		System.out.println("Hello to Skibitana Catalog lite");
-
+		
 		System.out.println("Please Provide an ID: ");
 		int myID = myScanner.nextInt();
+		
 		System.out.println("Please Enter Password: ");
 		Scanner myScanner2 = new Scanner(System.in);
 		String myPass = myScanner2.nextLine();
 		//Exit Scanner
-		myScanner2.close();
-		myScanner.close();
+		//myScanner.close();
+		//myScanner2.close();
 		
 		// DB Search
 		try {
-			for (int i = 0; i < UserList.size(); i++) {
-				if (UserList.get(i).ID == myID) {
+			for (int i = 0; i < Users.UserList.size(); i++) {
+				
+				Users myUser = Users.UserList.get(i);
+				int foundID = myUser.getID();
+				
+				if (foundID == myID) {
 					
 					// Pass Handling
-					
-					if (UserList.get(i).Password.equals(myPass)) {
+					String foundPass = myUser.getPass();
+					if (foundPass.equals(myPass)) {
+						
+						myUser.pf();
+						myUser.actions();
+						Scanner myScanner3 = new Scanner(System.in);
+						System.out.println("Type Action Number: ");
 
-						System.out.println("Logged In");
-						showCreds(UserList.get(i));
+						int action = myScanner3.nextInt();
+						
+						myUser.action_action(action);
+						if (action==1) {
+							Administration fakeUser2 = new Administration("Spoonos", "Spiniss@gmail.com", "Spanos", (byte) 3,1);
+							Users.UserList.add(fakeUser2);
+						}
+						myScanner.close();
+						myScanner2.close();
+						myScanner3.close();
 					}
 
 					else {
